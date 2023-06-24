deb6c4f0f98da5c02393368ba8e1a26ba542ec5e
vanikthai <vanikthai@gmail.com>
2023-06-24 18:16:43 +0700

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser())
-app.use(ejslay);
+app.use(ejslay); 
 app.use(flash())
 app.use(
   session({
